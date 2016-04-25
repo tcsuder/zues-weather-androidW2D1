2597fd0be04ae4cf0025c1320736fa77b38c278d
chris young and tyler suderman <tcsuder@gmail.com>
2016-04-25 16:51:40 -0700

    setup Forecast class with member variables

---
+++ app/src/main/java/com/epicodus/zeus/Forecast.java
@@ -0,0 +1,16 @@
+package com.epicodus.zeus;
+
+/**
+ * Created by Guest on 4/25/16.
+ */
+public class Forecast {
+    private Double mTime;
+    private Double mTempMin;
+    private Double mTempMax;
+    private Double mHumidity;
+    private String mWeatherMain;
+    private String mWeatherDescription;
+    private String mWeatherIcon;
+    private Double mWindSpeed;
+    private Double mClouds;
+}
