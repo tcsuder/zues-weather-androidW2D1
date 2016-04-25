636182627362914e9b2d688ac05ec4b0c1fcbcf7
chris young and tyler suderman <ctopheryoung@gmail.com>
2016-04-25 11:07:29 -0700

    pass location to weather acitivity

--- app/src/main/java/com/epicodus/zeus/MainActivity.java
+++ app/src/main/java/com/epicodus/zeus/MainActivity.java
@@ -1,13 +1,36 @@
 package com.epicodus.zeus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.locationSubmit) Button mLocationSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mLocationSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLocationSubmit) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/epicodus/zeus/WeatherActivity.java
@@ -0,0 +1,24 @@
+package com.epicodus.zeus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import butterknife.ButterKnife;
+
+public class WeatherActivity extends AppCompatActivity {
+    public static final String TAG = WeatherActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+
+        Log.i(TAG, "YOUR LOCATION IS " + location + ".");
+    }
+}
